240616c4ace097c046f577c9804b757ea131eced
Felipe Álvarez <falvarez@votainteligente.cl>
2014-09-10 15:27:13 -0300

    Using MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='vizard_graphs',
       version='0.0.1',
       packages=['vizard_graphs'],
-      package_data = {'vizard_graphs': ['templates/*.html','static/*',],},
+#      package_data = {'vizard_graphs': ['templates/*','static/*',],},
       license='MIT',
       author='Ciudadano Inteligente',
       author_email='lab@ciudadanointeligente.org',
